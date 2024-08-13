9bc15cf334c572320f0707e08b5bca174618f2ae
AaronWongNSC <31226232+AaronWongNSC@users.noreply.github.com>
2024-08-13 09:46:32 -0700

    Add files via upload

--- sample_strategy_1.py
+++ sample_strategy_1.py
@@ -1,7 +1,5 @@
 def strategy(age, amount):
     if age < 40:
         return 0, 0.2*amount, 0.8*amount
-    if age >= 40 and age < 60:
-        return 0, 0.5*amount, 0.5*amount
-    if age >= 60:
+    if age >= 40:
         return 0.333*amount, 0.333*amount, 0.334*amount
